33ef8c943c7d295f7e3f259c4a5ce24f27f9a00f
najafzadacavad <najafzadacavad@icloud.com>
2026-04-02 18:08:09 +0300

    version 1.4

---
+++ script.js
@@ -0,0 +1,54 @@
+const jsonData = `{
+"hello":{"tr":"merhaba","az":"salam"},
+"world":{"tr":"dunya","az":"dunya"},
+"computer":{"tr":"bilgisayar","az":"komputer"},
+"student":{"tr":"ogrenci","az":"telebe"}
+}`;
+
+class Translator{
+
+constructor(){
+this.dictionary = JSON.parse(jsonData);
+}
+
+getTranslation(text,lang){
+
+if(this.dictionary[text]){
+
+const {tr,az} = this.dictionary[text];
+
+if(lang==="tr"){
+return tr;
+}else{
+return az;
+}
+
+}
+
+return "Translation not found";
+
+}
+
+}
+
+const translator = new Translator();
+
+const fakeAsync = () =>{
+return new Promise((resolve)=>{
+resolve();
+});
+}
+
+const translateText = async () =>{
+
+await fakeAsync();
+
+let text = document.getElementById("inputText").value.toLowerCase();
+
+let lang = document.getElementById("language").value;
+
+let result = translator.getTranslation(text,lang);
+
+document.getElementById("result").innerHTML = result;
+
+}
